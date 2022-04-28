dd4d923ab99af5142b5c056a185d94d81f910b0c
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2022-04-28 20:11:11 +0200

    MOTD+StatTrack 2.1.0

--- DRGPacker/Scripts/Execute.bat
+++ DRGPacker/Scripts/Execute.bat
@@ -3,5 +3,6 @@
 @call %scriptDir%\Vars.bat
 @call %scriptDir%\_MoveFilesToInput.bat
 @call %scriptDir%\_Repack.bat
+"%PROGRAMFILES%/7-Zip/7z.exe" a -tzip Input.zip Input.pak
 @call %scriptDir%\_MovePakToMods.bat
 @call %scriptDir%\_RunGame.bat
